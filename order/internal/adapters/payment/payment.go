487ceb0a235b61b662c598f5cb892d3a2d15ee72
aknEvrnky <evrenkaya.2002.save@gmail.com>
2024-06-26 19:04:17 +0300

    add a retry policy for payment service

--- order/internal/adapters/payment/payment.go
+++ order/internal/adapters/payment/payment.go
@@ -4,36 +4,45 @@ import (
 	"context"
 	"github.com/aknEvrnky/grpc-microservices-proto/golang/payment"
 	"github.com/aknevrnky/grpc-microservices/order/internal/application/core/domain"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"time"
 )
 
 type Adapter struct {
-	client payment.PaymentClient
+	payment payment.PaymentClient
 }
 
 func NewAdapter(paymentServiceUrl string) (*Adapter, error) {
 	var opts []grpc.DialOption
+	// Retry policy for gRPC client, it will retry 5 times if the error is Unavailable or ResourceExhausted
+	opts = append(opts, grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(
+		grpc_retry.WithCodes(codes.Unavailable, codes.ResourceExhausted),
+		grpc_retry.WithMax(5),
+		grpc_retry.WithBackoff(grpc_retry.BackoffLinear(time.Second)),
+	)))
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	conn, err := grpc.NewClient(paymentServiceUrl, opts...)
 	if err != nil {
 		return nil, err
 	}
-	defer conn.Close()
 
 	return &Adapter{
-		client: payment.NewPaymentClient(conn),
+		payment: payment.NewPaymentClient(conn),
 	}, nil
 }
 
 func (a *Adapter) Charge(order *domain.Order) error {
-	req := payment.CreatePaymentRequest{
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	_, err := a.payment.Create(ctx, &payment.CreatePaymentRequest{
 		UserId:     order.CustomerID,
 		OrderId:    order.ID,
 		TotalPrice: order.TotalPrice(),
-	}
-
-	_, err := a.client.Create(context.Background(), &req)
+	})
 
 	return err
 }
